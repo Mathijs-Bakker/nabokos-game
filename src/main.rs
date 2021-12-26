c223198da49c080becca29aee56c118aa88255a4
Mathijs Bakker <mathijs_bakker@me.com>
2021-12-26 16:59:53 +0100

    Add components and entities

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,104 @@
 use ggez::{conf, event, Context, GameResult};
 use std::path;
 
+use specs::{ Component,Builder, World, WorldExt, VecStorage};
+
+// Components:
+
+#[derive(Debug, Component, Clone, Copy)]
+#[storage(VecStorage)]
+pub struct Position {
+    x: u8,
+    y: u8,
+    z: u8,
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Renderable {
+    path: String,
+}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Wall {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Player {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct Box {}
+
+#[derive(Component)]
+#[storage(VecStorage)]
+pub struct BoxSpot {}
+
+pub fn register_components(world: &mut World) {
+    world.register::<Position>();
+    world.register::<Renderable>();
+    world.register::<Player>();
+    world.register::<Wall>();
+    world.register::<Box>();
+    world.register::<BoxSpot>();
+}
+
+// Entities:
+
+pub fn create_wall(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/wall.png".to_string(),
+        })
+        .with(Wall {})
+        .build();
+}
+
+pub fn create_floor(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 5, ..position })
+        .with(Renderable {
+            path: "/images/floor.png".to_string(),
+        })
+        .build();
+}
+
+pub fn create_box(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/box.png".to_string(),
+        })
+        .with(Box {})
+        .build();
+}
+
+pub fn create_box_spot(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 9, ..position })
+        .with(Renderable {
+            path: "/images/box_spot.png".to_string(),
+        })
+        .with(BoxSpot {})
+        .build();
+}
+
+pub fn create_player(world: &mut World, position: Position) {
+    world
+        .create_entity()
+        .with(Position { z: 10, ..position })
+        .with(Renderable {
+            path: "/images/player.png".to_string(),
+        })
+        .with(Player {})
+        .build();
+}
 // This struct will hold all our game state
 // For now there is nothing to be held, but we'll add
 // things shortly.
